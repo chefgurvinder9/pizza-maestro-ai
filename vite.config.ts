097b42e7d22c85dd0eba9f762d6fcd33fd9fd441
chefgurvinder9 <chefgurvinder99@gmail.com>
2026-01-11 22:17:11 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // ADD THIS LINE BELOW (Replace YOUR_REPO_NAME with your actual repo name)
+  base: "/Pizza-Dough-Calci/", 
   server: {
     host: "::",
     port: 8080,
